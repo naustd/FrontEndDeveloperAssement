31150c95551b34cd10832a0898fabe2d99befd20
naustd <157865977+naustd@users.noreply.github.com>
2025-11-01 23:16:45 +0000

    feat(pdf): start designing PDF layout & structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  // https://vitejs.dev/config/
-  export default defineConfig({
-      plugins: [react()],
-  });
-  
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ["localhost", ".csb.app"],
+    port: 5173,
+  },
+});
